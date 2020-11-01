0c21eb751bd0376cae5f1214d359cb51195f520a
Pierre Dejoue <pierre.dejoue@gmail.com>
2020-11-01 19:27:34 +0100

    Improve maintainability
    - Improve comments
    - Homogenize notations
    - Remove some dead code

--- poly2tri/sweep/sweep.h
+++ poly2tri/sweep/sweep.h
@@ -119,9 +119,10 @@ private:
   bool Legalize(SweepContext& tcx, Triangle& t);
 
   /**
+   * <b>Determines if d is inside the circumcircle of triangle abc</b><br>
    * <b>Requirement</b>:<br>
    * 1. a,b and c form a triangle.<br>
-   * 2. a and d is know to be on opposite side of bc<br>
+   * 2. a and d are known to be on opposite side of bc<br>
    * <pre>
    *                a
    *                +
@@ -130,17 +131,17 @@ private:
    *            b/     \c
    *            +-------+
    *           /    d    \
-   *          /           \
+   *          /     +     \
    * </pre>
-   * <b>Fact</b>: d has to be in area B to have a chance to be inside the circle formed by
-   *  a,b and c<br>
-   *  d is outside B if orient2d(a,b,d) or orient2d(c,a,d) is CW<br>
+   * <b>Fact</b>: d has to be in area A delimited by semi-lines ab and ac to have a chance to be inside
+   * the circumcircle of triangle abc<br>
+   *  d is outside A if orient2d(a,b,d) or orient2d(c,a,d) is CW<br>
    *  This preknowledge gives us a way to optimize the incircle test
-   * @param a - triangle point, opposite d
+   * @param a - triangle point, opposite to d
    * @param b - triangle point
    * @param c - triangle point
-   * @param d - point opposite a
-   * @return true if d is inside circle, false if on circle edge
+   * @param d - point opposite to a
+   * @return true if d is inside the circumcircle of triangle abc, false if d is on or outside that circle
    */
   bool Incircle(const Point& pa, const Point& pb, const Point& pc, const Point& pd) const;
 
